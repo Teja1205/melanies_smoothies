2dc80d0245a59b73a726aa8b5f76c9e5029a2d08
Teja1205 <154227865+Teja1205@users.noreply.github.com>
2024-03-14 20:07:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
@@ -25,14 +24,13 @@ ingredients_list = st.multiselect('Choose up to 5 ingredients:'
 
 if ingredients_list:
     ingredients_string=''
+    
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen+' '
-  #  st.write(ingredients_string)
-
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-   # st.write(my_insert_stmt)
-    #st.stop()
+        st.subheader(fruit_chosen+ 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+        
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
