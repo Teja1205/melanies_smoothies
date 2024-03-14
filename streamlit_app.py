25308dbaaa58530faf14715c61ea85350492d625
Teja1205 <154227865+Teja1205@users.noreply.github.com>
2024-03-14 23:02:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,13 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         
         ingredients_string +=fruit_chosen+' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(fruit_chosen+ 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-       # search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-       # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-     #   
-      #  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
-      #  fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+
         
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
